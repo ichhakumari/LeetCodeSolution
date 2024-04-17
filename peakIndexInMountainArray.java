1b11b6ab118fc6f68879f2e6de1c601dea96b533
Ichha kumari <143002729+ichhakumari@users.noreply.github.com>
2024-04-17 19:27:21 +0530

    Update peakIndexInMountainArray.java

--- peakIndexInMountainArray.java
+++ peakIndexInMountainArray.java
@@ -1,1 +1,13 @@
 //Q.852 Peek index in a mountain array
+
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+      int low=0;
+      int high = arr.length -1;  
+
+      while(low<high){
+        int mid= low+(high-low)/2;
+
+        if  (arr[mid] < arr[mid+1]){
+            low=mid+1;
+        }
