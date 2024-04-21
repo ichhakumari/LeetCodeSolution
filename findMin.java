59ad69dcaeab543b069707f86ff324de9ca833db
Ichha kumari <143002729+ichhakumari@users.noreply.github.com>
2024-04-21 19:55:57 +0530

    Update findMin.java

--- findMin.java
+++ findMin.java
@@ -1,3 +1,21 @@
 /**Q.153 Suppose an array of length n sorted in ascending order is rotated between 1 and n times. 
   For example, the array nums = [0,1,2,4,5,6,7] might become*/
 
+class Solution {
+    public int findMin(int[] nums) {
+        int low=0;
+        int high= nums.length -1;
+
+        while(low<high){
+           int mid= low+(high-low)/2;
+
+            if(nums[mid] > nums[high]){
+                low=mid+1;
+            }
+            else{
+                high=mid;
+            }
+        }
+        return nums[high];
+    }
+}
