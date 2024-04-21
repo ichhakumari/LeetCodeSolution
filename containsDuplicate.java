497cb269ef8cbe2b880b4908a36127f68363a16d
Ichha kumari <143002729+ichhakumari@users.noreply.github.com>
2024-04-21 19:52:15 +0530

    Update containsDuplicate.java

--- containsDuplicate.java
+++ containsDuplicate.java
@@ -1,5 +1,5 @@
-//Q217. Given an integer array nums, return true if any value appears at
-//least twice in the array, and return false if every element is distinct.
+/**Q217. Given an integer array nums, return true if any value appears at
+least twice in the array, and return false if every element is distinct.*/
 
 class Solution {
     public boolean containsDuplicate(int[] nums) {
