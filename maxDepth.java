39ad007e0a9340833519c2b39490a6071caad9ce
Ichha kumari <143002729+ichhakumari@users.noreply.github.com>
2024-04-23 21:42:50 +0530

    Update maxDepth.java

--- maxDepth.java
+++ maxDepth.java
@@ -1,1 +1,27 @@
+public class Solution {
 
+    public int maxDepth(String s) {
+        int maxDepth = 0;
+        int currentDepth = 0;
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                currentDepth++;
+                maxDepth = Math.max(maxDepth, currentDepth);
+            } else if (c == ')') {
+                if (currentDepth == 0) {
+                    // Mismatched closing parenthesis, invalid string
+                    return -1;
+                }
+                currentDepth--;
+            }
+        }
+
+        // Check if all opening parentheses are closed
+        if (currentDepth > 0) {
+            return -1;
+        }
+
+        return maxDepth;
+    }
+}
