5d47672a463ce475a0ee67186af34348a8b1d56b
Ichha kumari <143002729+ichhakumari@users.noreply.github.com>
2024-04-30 20:01:35 +0530

    Update majorityElementII.java

--- majorityElementII.java
+++ majorityElementII.java
@@ -3,3 +3,43 @@
 The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
 
  */
+
+import java.util.*;
+class Solution {
+    public static int majorityElement(int[] nums) {
+    
+        //size of the given array:
+        int n = nums.length;
+        int cnt = 0; // count
+        int el = 0; // Element
+
+        //applying the algorithm:
+        for (int i = 0; i < n; i++) {
+            if (cnt == 0) {
+                cnt = 1;
+                el = nums[i];
+            } else if (el == nums[i]) cnt++;
+            else cnt--;
+        }
+
+        //checking if the stored element
+        // is the majority element:
+        int cnt1 = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == el) cnt1++;
+        }
+
+        if (cnt1 > (n / 2)) return el;
+        return -1;
+    }
+
+    public static void main(String args[]) {
+        int[] arr = {2, 2, 1, 1, 1, 2, 2};
+        int ans = majorityElement(arr);
+        System.out.println("The majority element is: " + ans);
+
+    }
+
+}
+
+
