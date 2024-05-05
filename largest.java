e01819f5aa747e37216df9a0b1aac8bb9dde914f
Ichha kumari <143002729+ichhakumari@users.noreply.github.com>
2024-05-05 18:31:23 +0530

    Update largest.java

--- largest.java
+++ largest.java
@@ -1,3 +1,18 @@
-/** Find the Largest element in an array*/
+/** Find the Largest element in an array......
+  
+Solution1: Sorting
+  
+Intuition:
+We can sort the array in ascending order, hence the largest element will be at the last index of the array. 
 
+Approach: 
+Sort the array in ascending order.
+Print the (size of the array -1)th index.
+DryRun: 
+Before sorting: arr[] = {2,5,1,3,0};
+
+After sorting: arr[] = {0,1,2,3,5};
+
+Hence answer : arr[sizeofthearray-1] =5
+  */
 
