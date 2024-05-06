a084c70cbfccfee1a6a755f60a3099fe9594ec14
Ichha kumari <143002729+ichhakumari@users.noreply.github.com>
2024-05-06 20:27:00 +0530

    Update KokoEatingBanana.java

--- KokoEatingBanana.java
+++ KokoEatingBanana.java
@@ -7,3 +7,51 @@ Koko likes to eat slowly but still wants to finish eating all the bananas before
 Return the minimum integer k such that she can eat all the bananas within h hours.
 */
 
+import java.util.*;
+
+public class Solution {
+    public static int findMax(int[] piles) {
+        int maxi = Integer.MIN_VALUE;;
+        int n = piles.length;
+        //find the maximum:
+        for (int i = 0; i < n; i++) {
+            maxi = Math.max(maxi, piles[i]);
+        }
+        return maxi;
+    }
+
+    public static int calculateTotalHours(int[] piles, int hourly) {
+        int totalH = 0;
+        int n = piles.length;
+        //find total hours:
+        for (int i = 0; i < n; i++) {
+            totalH += Math.ceil((double)(piles[i]) / (double)(hourly));
+        }
+        return totalH;
+    }
+
+    public static int minEatingSpeed(int[] piles, int h) {
+        int low = 1, high = findMax(piles);
+
+        //apply binary search:
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int totalH = calculateTotalHours(piles, mid);
+            if (totalH <= h) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+
+    public static void main(String[] args) {
+        int[] piles = {7, 15, 6, 3};
+        int h = 8;
+        int ans = minEatingSpeed(piles, h);
+        System.out.println("Koko should eat at least " + ans + " bananas/hr.");
+    }
+}
+
+
