606c0dc8b4a0966cf082736f4b78218f60e59af7
Ichha kumari <143002729+ichhakumari@users.noreply.github.com>
2024-05-08 21:20:35 +0530

    Update mySqrt.java

--- mySqrt.java
+++ mySqrt.java
@@ -2,3 +2,38 @@
 
 You must not use any built-in exponent function or operator. */
 
+public class Solution {
+
+    public static int mySqrt(int x) {
+        if (x == 0) {
+            return 0;
+        }
+
+        // Handle corner cases for very large numbers
+        if (x > 46340 * 46340) {
+            return 46340; 
+        }
+
+        long low = 1;
+        long high = x;
+        while (low <= high) {
+            long mid = low + (high - low) / 2;
+            if (mid * mid == x) {
+                return (int) mid; 
+            } else if (mid * mid < x) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        // Rounding down using long arithmetic
+        return (int) high;
+    }
+
+    public static void main(String[] args) {
+        int num = 2147483647; 
+        int sqrt = mySqrt(num);
+        System.out.println("The square root of " + num + " (rounded down) is: " + sqrt);
+    }
+}
