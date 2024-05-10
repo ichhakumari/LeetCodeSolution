fa74dbbdd2fc3edbd0eed4e36f14d56bf44dc2cc
Ichha kumari <143002729+ichhakumari@users.noreply.github.com>
2024-05-10 20:29:01 +0530

    Update isPalindrome.java

--- isPalindrome.java
+++ isPalindrome.java
@@ -1,1 +1,36 @@
 //Q 234  Palindrome Linked list
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+       
+        fast = head;
+        ListNode prev = null;
+        while (slow != null) {
+            ListNode next = slow.next;
+            slow.next = prev;
+            prev = slow;
+            slow = next;
+        }
+
+        
+        slow = prev;
+        while (slow != null) {
+            if (slow.val != fast.val) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        return true;
+    }
+}
+
