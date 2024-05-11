c10a50f0f67acd2c318e1df66aa893284d3bf7e0
Ichha kumari <143002729+ichhakumari@users.noreply.github.com>
2024-05-11 20:33:33 +0530

    Update middleNode.java

--- middleNode.java
+++ middleNode.java
@@ -1,5 +1,6 @@
-//  Middle of linked list
-/**
+/** Middle of linked list:  Given the head of a singly linked list, return the middle node of the linked list.
+    If there are two middle nodes, return the second middle node.
+
  * Definition for singly-linked list.
  * public class ListNode {
  *     int val;
