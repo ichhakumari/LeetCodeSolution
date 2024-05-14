f96d295a59c657442f40f444bfa70c3baef833e7
Ichha kumari <143002729+ichhakumari@users.noreply.github.com>
2024-05-14 14:29:50 +0530

    Update deleteMiddle.java

--- deleteMiddle.java
+++ deleteMiddle.java
@@ -1,4 +1,4 @@
-/**   You are given the head of a linked list. <b>Delete the middle node</b>, and return the head of the modified linked list.
+/**   You are given the head of a linked list. DELETE THE MIDDLE NODE, and return the head of the modified linked list.
 
 The middle node of a linked list of size n is the ⌊n / 2⌋th node from the start using 0-based indexing, where ⌊x⌋ denotes the largest integer less than or equal to x.
 
