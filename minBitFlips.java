3186df1d935335da313a5e344d70be118b4148b8
Ichha kumari <143002729+ichhakumari@users.noreply.github.com>
2024-05-23 21:06:24 +0530

    Update minBitFlips.java

--- minBitFlips.java
+++ minBitFlips.java
@@ -1,2 +1,15 @@
 /** MINIMUM BITS FLIPS TO CONVERT A NUMBER
 */
+class Solution {
+    public int minBitFlips(int start, int goal) {
+        int ans = start ^ goal;
+        int cnt = 0;
+        
+        while(ans != 0){
+            ans = ans & (ans -1);
+            cnt = cnt +1;
+        }
+        
+        return cnt;
+    }
+  }
