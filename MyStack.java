3bc95133e875a843bc2e3826c9f12ae7aec12fb1
Ichha kumari <143002729+ichhakumari@users.noreply.github.com>
2024-05-24 14:58:10 +0530

    Update MyStack.java

--- MyStack.java
+++ MyStack.java
@@ -1,1 +1,25 @@
+class MyStack {
+
+  private Queue<Integer> q = new LinkedList<>();  
+  public void push(int x) {
+    q.add(x);
+    for (int i = 0; i < q.size() - 1; i++) {
+      q.add(q.remove());  
+    }
+  }
+
+  public int pop() {  
+    if (empty()) return -1;
+    return q.remove();
+  }
+
+  public int top() {
+    if (empty()) return -1;
+    return q.peek();
+  }
+
+  public boolean empty() {
+    return q.isEmpty();
+  }
+}
 
