bd74ac50ac8fd3bb1fd0b3f0eda572207e02f545
Ichha kumari <143002729+ichhakumari@users.noreply.github.com>
2024-05-25 21:42:09 +0530

    Update MyQueue.java

--- MyQueue.java
+++ MyQueue.java
@@ -1,3 +1,3 @@
 /**   java code for Implement a first in first out (FIFO) queue using only two stacks. 
-The implemented queue should support all the functions of a normal queue (push, peek, pop, and empty).  
+      The implemented queue should support all the functions of a normal queue (push, peek, pop, and empty).  
 */
