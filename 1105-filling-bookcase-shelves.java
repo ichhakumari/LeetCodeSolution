d9f0e17585b4ce7b1050838cc6509f5d6ae2b1ce
Ichha kumari <143002729+ichhakumari@users.noreply.github.com>
2024-07-31 20:11:15 +0530

    Update 1105-filling-bookcase-shelves.java

--- 1105-filling-bookcase-shelves.java
+++ 1105-filling-bookcase-shelves.java
@@ -1,3 +1,8 @@
+/**  You are given an array books where books[i] = [thicknessi, heighti] indicates the thickness and height of the ith book. You are also given an integer shelfWidth.
+We want to place these books in order onto bookcase shelves that have a total width shelfWidth.
+We choose some of the books to place on this shelf such that the sum of their thickness is less than or equal to shelfWidth, then build another level of the shelf of the bookcase so that the total height of the bookcase has increased by the maximum height of the books we just put down. We repeat this process until there are no more books to place.
+*/
+
 class Solution {
     public int minHeightShelves(int[][] books, int shelfWidth) {
         int len = books.length;
