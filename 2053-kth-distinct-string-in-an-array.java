9001cea146f16b1ff3729d0ee555bdf238586bad
Ichha kumari <143002729+ichhakumari@users.noreply.github.com>
2024-08-05 20:53:04 +0530

    Update 2053-kth-distinct-string-in-an-array.java

--- 2053-kth-distinct-string-in-an-array.java
+++ 2053-kth-distinct-string-in-an-array.java
@@ -5,3 +5,26 @@ Given an array of strings arr, and an integer k, return the kth distinct string 
 
 Note that the strings are considered in the order in which they appear in the array.
   */
+
+class Solution {
+    public String kthDistinct(String[] arr, int k) {
+       
+        Map<String, Integer> frequencyMap = new HashMap<>();
+      
+        for (String element : arr) {
+            frequencyMap.put(element, frequencyMap.getOrDefault(element, 0) + 1);
+        }
+      
+        for (String element : arr) {
+            if (frequencyMap.get(element) == 1) {
+                k--; 
+   
+                if (k == 0) {
+                    return element;
+                }
+            }
+        }
+      
+        return "";
+    }
+}
