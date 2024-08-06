67e626fe23d7182d3a45e48631436363f5721d4e
Ichha kumari <143002729+ichhakumari@users.noreply.github.com>
2024-08-06 23:15:16 +0530

    Update 3016-minimum-number-of-pushes-to-type-word-ii.java

--- 3016-minimum-number-of-pushes-to-type-word-ii.java
+++ 3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -1,7 +1,13 @@
 /** 3016-minimum-number-of-pushes-to-type-word-ii.java
+
+You are given a string word containing lowercase English letters.
+Telephone keypads have keys mapped with distinct collections of lowercase English letters, which can be used to form words by pushing them. For example,
+the key 2 is mapped with ["a","b","c"], we need to push the key one time to type "a", two times to type "b", and three times to type "c" .
+It is allowed to remap the keys numbered 2 to 9 to distinct collections of letters. The keys can be remapped to any amount of letters, but each letter
+must be mapped to exactly one key. You need to find the minimum number of times the keys will be pushed to type the string word.
   */
+
 class Solution {
-  
   public int minimumPushes(String word) {
     int ans = 0;
     int[] count = new int[26];
