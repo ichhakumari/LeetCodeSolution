c3d13580ceb8ee010a588c3963cd4fbf51058a33
Ichha kumari <143002729+ichhakumari@users.noreply.github.com>
2024-08-12 20:08:52 +0530

    Update 703. Kth Largest Element in a Stream.java

--- 703. Kth Largest Element in a Stream.java
+++ 703. Kth Largest Element in a Stream.java
@@ -1,2 +1,27 @@
 /** 703. Kth Largest Element in a Stream
   */
+import java.util.PriorityQueue;
+class KthLargest {
+    private PriorityQueue<Integer> minHeap;
+    private int k;
+
+    public KthLargest(int k, int[] nums) {
+       
+        this.minHeap = new PriorityQueue<>(k);
+        this.k = k;
+
+        for (int num : nums) {
+            add(num);
+        }
+    }
+    public int add(int val) {
+      
+        minHeap.offer(val);
+
+        if (minHeap.size() > k) {
+            minHeap.poll();
+        }
+        return minHeap.peek();
+    }
+}
+
