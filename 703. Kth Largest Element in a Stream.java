e6ebc5012921bba3bd561693247734fafec9a5f8
Ichha kumari <143002729+ichhakumari@users.noreply.github.com>
2024-08-12 20:09:16 +0530

    Update 703. Kth Largest Element in a Stream.java

--- 703. Kth Largest Element in a Stream.java
+++ 703. Kth Largest Element in a Stream.java
@@ -1,5 +1,12 @@
 /** 703. Kth Largest Element in a Stream
+Design a class to find the kth largest element in a stream. Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+Implement KthLargest class:
+
+KthLargest(int k, int[] nums) Initializes the object with the integer k and the stream of integers nums.
+int add(int val) Appends the integer val to the stream and returns the element representing the kth largest element in the stream.
   */
+
 import java.util.PriorityQueue;
 class KthLargest {
     private PriorityQueue<Integer> minHeap;
