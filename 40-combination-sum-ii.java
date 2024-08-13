32e6b7da30b40095c1f873ae59c0b78e7879637a
Ichha kumari <143002729+ichhakumari@users.noreply.github.com>
2024-08-13 14:34:17 +0530

    Update 40-combination-sum-ii.java

--- 40-combination-sum-ii.java
+++ 40-combination-sum-ii.java
@@ -1,4 +1,11 @@
 // 40-combination-sum-ii
+// Given a collection of candidate numbers (candidates) and a target number (target), find all unique combinations in candidates where the candidate numbers sum to target.
+
+// Each number in candidates may only be used once in the combination.
+
+// Note: The solution set must not contain duplicate combinations.
+
+  
 class Solution {
   public List<List<Integer>> combinationSum2(int[] candidates, int target) {
     List<List<Integer>> ans = new ArrayList<>();
