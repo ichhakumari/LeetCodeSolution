577ebbbcfa8d897134b16b1c22736ec3362a1e6c
Ichha kumari <143002729+ichhakumari@users.noreply.github.com>
2024-08-22 21:27:24 +0530

    Update 476-number-complement.java

--- 476-number-complement.java
+++ 476-number-complement.java
@@ -4,3 +4,10 @@
 // Given an integer num, return its complement.
 
 
+class Solution {
+  public int findComplement(int num) {
+    for (long i = 1; i <= num; i <<= 1)
+      num ^= i;
+    return num;
+  }
+}
