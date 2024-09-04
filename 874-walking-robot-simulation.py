8ba9a7b6eec6d7288f0df951d9d2f9272f21e997
Ichha kumari <143002729+ichhakumari@users.noreply.github.com>
2024-09-04 20:58:01 +0530

    Update 874-walking-robot-simulation.py

--- 874-walking-robot-simulation.py
+++ 874-walking-robot-simulation.py
@@ -1,3 +1,10 @@
+# A robot on an infinite XY-plane starts at point (0, 0) facing north. The robot can receive a sequence of these three possible types of commands:
+
+# -2: Turn left 90 degrees.
+# -1: Turn right 90 degrees.
+# 1 <= k <= 9: Move forward k units, one unit at a time.
+# Some of the grid squares are obstacles. The ith obstacle is at grid point obstacles[i] = (xi, yi). If the robot runs into an obstacle, then it will instead stay in its current location and move on to the next command.
+
 class Solution:
     def robotSim(self, commands: List[int], obstacles: List[List[int]]) -> int:
         dirs = (0, 1, 0, -1, 0)
