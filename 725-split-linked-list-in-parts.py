35e2db80c6c9b5fe2b6167aa428d5e243dcd93ee
Ichha kumari <143002729+ichhakumari@users.noreply.github.com>
2024-09-08 20:01:09 +0530

    Update 725-split-linked-list-in-parts.py

--- 725-split-linked-list-in-parts.py
+++ 725-split-linked-list-in-parts.py
@@ -1,7 +1,38 @@
-Given the head of a singly linked list and an integer k, split the linked list into k consecutive linked list parts.
+# Given the head of a singly linked list and an integer k, split the linked list into k consecutive linked list parts.
 
-The length of each part should be as equal as possible: no two parts should have a size differing by more than one. This may lead to some parts being null.
+# The length of each part should be as equal as possible: no two parts should have a size differing by more than one. This may lead to some parts being null.
 
-The parts should be in the order of occurrence in the input list, and parts occurring earlier should always have a size greater than or equal to parts occurring later.
+# The parts should be in the order of occurrence in the input list, and parts occurring earlier should always have a size greater than or equal to parts occurring later.
 
-Return an array of the k parts
+# Return an array of the k parts
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+  def splitListToParts(self, root: ListNode, k: int) -> list[ListNode]:
+    ans = [[] for _ in range(k)]
+    length = 0
+    curr = root
+    while curr:
+      length += 1
+      curr = curr.next
+    subLength = length // k
+    remainder = length % k
+
+    prev = None
+    head = root
+
+    for i in range(k):
+      ans[i] = head
+      for j in range(subLength + (1 if remainder > 0 else 0)):
+        prev = head
+        head = head.next
+      if prev:
+        prev.next = None
+      remainder -= 1
+
+    return ans
+        
